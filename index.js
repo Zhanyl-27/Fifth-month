92848d313dc233aebca499a47f89a95348b2dc50
Zhanyl-27 <108879757+Zhanyl-27@users.noreply.github.com>
2022-12-03 00:00:05 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,19 @@
 import React from "react";
-import HeaderContent from "../HeaderContent";
-import ParagraphContent from "../ParagraphContent";
-import UsersList from "../UsersList";
-import {ThemeContext, themes} from '../ThemeContext';
+import { WithResult } from "../hoc-helpers";
+import {ThemeContext} from '../ThemeContext';
+
+class HeaderContent extends React.Component {
+    render() 
+    {
+        let props = this.props;
+        let theme = this.context;
+        return (<>
+            <h1 style={{color: theme.foreground,backgroundColor: theme.background}}>App {props.result}</h1>
+        </>);
+    }
 
 
+};
+HeaderContent.contextType = ThemeContext;
 
-
-class App extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      theme: themes.light,
-    };
-
-    this.toggleTheme = () => {
-      this.setState(state => ({
-        theme:
-          state.theme === themes.dark
-            ? themes.light
-            : themes.dark,
-      }));
-    };
-  }
-    render (){
-       return(<>
-          <ThemeContext.Provider value={this.state.theme}>
-          <div style={{color: this.state.theme.foreground,backgroundColor: this.state.theme.background}}>
-             <HeaderContent a={15} b={90}/>
-             <button  style={{color: this.state.theme.foreground,backgroundColor: this.state.theme.background}} onClick={this.toggleTheme}>Тема</button>
-             <ParagraphContent a={123} b={455}/>
-             <hr/>
-             <UsersList/>
-             <hr/>
-             </div>
-          </ThemeContext.Provider>      
-       </>);
-    };
-}
-
-export default App;
+export default WithResult(HeaderContent);
